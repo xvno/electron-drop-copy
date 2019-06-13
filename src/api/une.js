46e9f985620cd3c2c6c051bc4312379ae3be76a5
volving <volving@icloud.com>
2019-06-13 19:28:44 +0800

    Add: basic ui, basic cmd

---
+++ src/api/une.js
@@ -0,0 +1,108 @@
+const electron = require('electron');
+const { ipcRenderer } = require('electron')
+
+
+module.exports.getName = function () {
+    return 'get a name';
+};
+
+// module.exports.showOpenDialog = function (fn) {
+//     ipcRenderer.send('showOpenDialog', '~/Desktop')
+// };
+
+module.exports.showSaveDialog = function () {
+    ipcRenderer.send('showSaveDialog', '~/Desktop')
+};
+
+
+const fs = require('fs');
+const { dialog } = electron.remote
+module.exports.openFile = function (fn) {
+    dialog.showOpenDialog(function (fileNames) {
+        if (fileNames === undefined) return;
+        var fileName = fileNames[0];
+        fs.realpath(fileName, function (err, realpath) {
+            console.log(realpath);
+        });
+        fs.readFile(fileName, 'utf-8', function (err, data) {
+            // console.log(data);
+            fn(data);
+        });
+    });
+}
+
+module.exports.saveFile = function (data, fn) {
+    dialog.showSaveDialog(function (fileName) {
+        if (fileName === undefined) return;
+        fs.writeFile(fileName, data, function (err) {
+            if (!err) {
+                dialog.showMessageBox({
+                    message: "文件已保存",
+                    buttons: ["OK"]
+                }, index => {
+                    console.log(index);
+                    fn();
+                });
+            }
+        });
+
+    });
+
+}
+// const SMB2 = require('@marsaud/smb2');
+
+// // create an SMB2 instance
+// const smb2Client = new SMB2({
+//     share: 'smbuser',
+//     domain: '192.168.1.180',
+//     username: 'smbuser',
+//     password: '1234',
+// });
+// module.exports.showSambaOpenDialog = function () {
+
+// };
+// module.exports.showSambaSaveDialog = function () {
+
+// };
+
+module.exports.dropHandler = function (event) {
+    console.log('File(s) dropped');
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < event.dataTransfer.items.length; i++) {
+            // If dropped items aren't files, reject them
+            if (event.dataTransfer.items[i].kind === 'file') {
+                console.log(event.dataTransfer.files[i]);
+                var file = event.dataTransfer.items[i].getAsFile();
+                console.log('... file[' + i + '].name = ' + file.name);
+            }
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < event.dataTransfer.files.length; i++) {
+            console.log(event.dataTransfer.files[i]);
+            console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
+        }
+    }
+}
+module.exports.ondragoverHandler = function (event) {
+    console.log('File(s) in drop zone');
+
+    // Prevent default behavior (Prevent file from being opened)
+    event.preventDefault();
+    event.stopPropagation();
+}
+
+
+const { exec } = require('child_process');
+const path = require('path');
+
+module.exports.sendCmd = function (files) {
+    files.forEach(file => {
+        let p = path.resolve(file.path, file.name)
+        console.log(`cp1 ${p} ${p + '.bak'}`);
+        exec(`cp1 ${p} ${p + '.bak'}`);
+    });
+}
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,176 @@
+<template>
+    <div id="wrapper">
+        <!-- <img id="logo" src="~@/assets/logo.png" alt="electron-vue"> -->
+        <main>
+            <div>
+                <button class="alt" @click="openFile()">Open</button>
+                <button class="alt" @click="saveFile()">Save</button>
+                <button class="alt" @click="showOpenGoDialog()">OpenGo</button>
+                <button class="alt" @click="showSaveGoDialog()">SaveGo</button>
+            </div>
+            <div class="doc" v-show="filedata.length > 0">
+                <p ref="data">{{filedata}}</p>
+            </div>
+            <p>
+                <label for="message">message:</label>
+                <input type="text" name="message" id="messageInput">
+                <input type="submit" value="Send" id="submit">
+            </p>
+            <p>
+                <label for="cmd">Commands</label>
+                <select name="cmd" id="cmdSelect">
+                    <option value="watch">watch</option>
+                    <option value="offwatch">offwatch</option>
+                    <option value="trans">trans</option>
+                    <option value="close">close</option>
+                </select>
+            </p>
+            <div class="dropzone" @drop="drop" @dragover="dragover"></div>
+        </main>
+    </div>
+</template>
+
+<script>
+import SystemInformation from './LandingPage/SystemInformation'
+import une from 'api/une'
+// import { dropHandler, ondragoverHandler } from 'api/due'
+
+export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    data() {
+        return {
+            name: '',
+            filedata: '',
+            files: []
+        }
+    },
+    created() {
+        this.name = une.getName()
+    },
+    methods: {
+        open(link) {
+            this.$electron.shell.openExternal(link)
+        },
+        openFile() {
+            console.log('showOpenDialog')
+            let z = this
+            une.openFile(function(data) {
+                z.filedata = data
+            })
+        },
+        saveFile() {
+            console.log('showSaveDialog')
+            // une.showSaveDialog({
+            //     title: '1st dialog window'
+            // })
+            let z = this
+            une.saveFile(this.filedata, function() {
+                z.filedata = ''
+            })
+        },
+        showOpenGoDialog() {},
+        showSaveGoDialog() {},
+        drop(event) {
+            console.log('File(s) dropped')
+            this.files = []
+            event.preventDefault()
+            event.stopPropagation()
+            if (event.dataTransfer.items) {
+                // Use DataTransferItemList interface to access the file(s)
+                for (var i = 0; i < event.dataTransfer.items.length; i++) {
+                    // If dropped items aren't files, reject them
+                    if (event.dataTransfer.items[i].kind === 'file') {
+                        console.log(event.dataTransfer.files[i])
+                        var file = event.dataTransfer.items[i].getAsFile()
+                        console.log('... file[' + i + '].name = ' + file.name)
+                        this.files.push({ name: file.name, path: file.path })
+                    }
+                }
+            } else {
+                // Use DataTransfer interface to access the file(s)
+                for (var i = 0; i < event.dataTransfer.files.length; i++) {
+                    console.log(event.dataTransfer.files[i])
+                    console.log(
+                        '... file[' +
+                            i +
+                            '].name = ' +
+                            event.dataTransfer.files[i].name
+                    )
+                    this.files.push({ name: file.name, path: file.path })
+                }
+            }
+            if (this.files.length > 0) {
+                une.sendCmd(this.files)
+            }
+        },
+        dragover(event) {
+            console.log('File(s) in drop zone')
+            // Prevent default behavior (Prevent file from being opened)
+            event.preventDefault()
+            event.stopPropagation()
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: 'Source Sans Pro', sans-serif;
+}
+
+#wrapper {
+    background: radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, 0.9) 100%
+    );
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+}
+
+#logo {
+    height: auto;
+    margin-bottom: 20px;
+    width: 420px;
+}
+
+main {
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+}
+
+button {
+    font-size: 0.8em;
+    cursor: pointer;
+    outline: none;
+    min-width: 6em;
+    padding: 0.45em 1em;
+    border-radius: 1em;
+    display: inline-block;
+    color: #fff;
+    background-color: #4fc08d;
+    transition: all 0.15s ease;
+    box-sizing: border-box;
+    border: 1px solid #4fc08d;
+}
+button.alt {
+    color: #42b983;
+    background-color: transparent;
+}
+.dropzone {
+    min-height: 400px;
+    background-color: antiquewhite;
+    border: 1px solid #678628;
+}
+</style>
